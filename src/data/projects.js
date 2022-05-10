adfd6818acf432e3a7823fa5a661cadfaecf606d
shan5742 <asamshan456@gmail.com>
2022-05-10 10:10:22 +0100

    adding project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,14 +1,23 @@
 export const data = [
   {
+    id: 0,
     title: "moonturds_xyz",
     url: "https://www.moonturds.xyz/",
     description:
       "Worked with Solidity lead engineer to develop the frontend minting website for a new NFT collection. Multi wallet support via wb3modal and wallet connect integration. Initial development locally and on the Rinkeby test network via Hardhat. Web3 integrations with ethers JS.",
   },
   {
+    id: 1,
     title: "Rinkedry?",
     url: "https://www.rinkedry.xyz/",
     description:
       "A Rinkedry faucet allowing users to request 0.05 Rinkeby ETH every 24 hours. In the effort to try and maintain the life and usability of the project it also accepts donations of 0.1 ETH. Top donators are ranked on the site to again add more incentive to donate. Supports ENS domains and verified avatars too",
+  },
+  {
+    id: 2,
+    title: "Simple Storage",
+    url: "https://shans-simple-storage.vercel.app/",
+    description:
+      "A simple Dapp and smart contracting allowing you to set and read a message from the blockchain. Deployed on the Rinkeby testnet and collates some other usage stats too",
   },
 ];
