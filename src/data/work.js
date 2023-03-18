8080970f3376763dd7ecb1acc21a5fa5d38b7333
shan5742 <asamshan456@gmail.com>
2023-03-18 06:34:56 +0000

    update CV

--- src/data/work.js
+++ src/data/work.js
@@ -3,9 +3,10 @@ export const data = [
     company: "Let's Eat",
     title: "Co-Founder & CTO",
     bullets: [
-      "Contributing in all stages of the technical product life cycle, including architecture and planning, development, testing and deployment, and ongoing maintenance.",
-      "Closely worked with tech leads to address tech debt, ensuring high-quality solutions were delivered in a timely manner while upholding company SLAs.",
-      "Identified and resolved any issues relating to hardware, software, and network infrastructure that were slowing down the operations of an organisation serving thousands of customers each year.",
+      "Contributed in all stages of the technical product life cycle, including architecture, planning, development, testing, deployment, and maintenance.",
+      "Direct and consult on technology choices and technical direction for all 4 LE applications.",
+      "Worked closely with tech leads to address tech debt and ensure timely delivery of high-quality solutions that were in line with company SLAs.",
+      "Identified and resolved issues related to hardware, software, and network infrastructure that impacted operations of an organization serving thousands of customers annually.",
     ],
   },
   {
@@ -28,9 +29,19 @@ export const data = [
     company: "Library of Things",
     title: "Software Engineer",
     bullets: [
-      "Helped scale the platform from 1 site to 3 sites in 6 months through building a custom CMS that automated a lot of content across various locations and channels.",
-      "Built out internal tools and protocols for a SaaS, supporting multiple admin permissions and levels.",
+      "Built a custom CMSwith Typescript and GraphQL that automated content across various locations and channels, enabling the platform to scale from 1 location to 4 in 6 months.",
+      "Built internal tools and protocols for a SaaS supporting multiple admin permissions and levels in preparation for LoT licensing their software.",
       "Supported development team in creating pipelines for continuous integration and continuous delivery using Gitlab alongside Docker.",
+    ],
+  },
+  {
+    company: "Northlink Digital",
+    title: "Software Engineer",
+    bullets: [
+      "Led a small team of developers to create scalable cloud first web and mobile applications. Reviews, mentoring, sprint planning and retros were all part of my responsibilities.",
+      "Delivered a fully-functional, React Native survey application promoting sexual and relationship education for students in Asia. The app is now rolled out across two continents, with multi-lingual support and used by thousands of students daily.",
+      "Worked on building out custom boiler plate using NextJS, SaaS and Sanity CMS to quickly spin up new client projects, which dramatically reduced dev time for  newer and junior devs.",
+      "Regularly attended  training courses and conferences.  Frequent code reviews with the CTO focussing on fast tracking my development skills",
     ],
   },
   {
@@ -41,31 +52,22 @@ export const data = [
     ],
   },
   {
-    company: "Northlink Digital",
-    title: "Software Engineer",
-    bullets: [
-      "Led a small team of developers to create scalable cloud first web and mobile applications. Reviews, mentoring, sprint planning and retros were all part of my responsibilities.",
-      "Delivered a fully-functional, React Native survey application promoting sexual and relationship education for students in Asia. The app is now rolled out across two continents, with multi-lingual support and used by thousands of students daily.",
-      "egularly attended  training courses and conferences.  Frequent code reviews with the CTO focussing on fast tracking my development skills",
-    ],
-  },
-  {
     company: "Coding Coach & The Mentoring Club",
     title: "Mentor",
     bullets: [
-      "Work with new developers to break into the software industry, providing mentorship and training on topics including React Hooks, state management, and portfolio development",
-      "Worked with new developers to break into the software industry, providing mentorship and training on topics including React Hooks, state management, and portfolio development",
-      "Three former mentees are now employed in the industry",
+      "Provide mentorship and training on topics including React Hooks, state management, and portfolio development to help new developers break into the software industry.",
+      "Discuss and help plan overall strategy for attacking learning and aggressively hunting for the first job. Once secured, I would work with mentees to develop a strategy to accelerate the early years of their career.",
+      "Five former mentees are now employed in the industry",
     ],
   },
   {
-    company: "Focus Digital",
-    title: "Consultant",
+    company: "Various",
+    title: "Writer",
     bullets: [
-      "Designed and built simple websites for small to medium sized companies using WordPress custom themes.",
-      "Manipulated custom theme with CSS.",
-      "Worked with a small freelance team including designer, developer, photographer and a project manager.",
-      "Used version control within the team to collaborate and release new features.",
+      "Extensive networking to secure clients.",
+      "Negotiated rates and deadlines for time sensitive projects..",
+      "Reporting and invoicing.",
+      "Built an online portfolio for better presence and to aid with securing new clients.",
     ],
   },
 ];
